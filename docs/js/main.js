8344c052331d19962f62ec15e43b865e6e108a83
ronnyrufino1 <ronny.rufino1@gmail.com>
2026-07-01 18:56:31 -0300

    correcao do endereco railway

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const BASE_URL = "pp4.railway.internal";
+  const BASE_URL = "https://pp4.railway.internal";
 
   let paginaAtual = 1;
   const itensPorPagina = 10;
